319a5c19cd352bfd9297ca491c6bf769e9308052
EduardoBragadeOliveira <eduardobragadeoliveira@gmail.com>
2022-07-13 14:44:14 -0400

    Códigos HTML/CSS/TypeScript

---
+++ assets/DIST/app.js
@@ -0,0 +1,60 @@
+"use strict";
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+let campoImposto = document.getElementById('campo-imposto');
+let saldoTot = 0;
+let impostoTot = 0;
+limparSaldo();
+limparImposto();
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTot += soma;
+        campoSaldo.innerHTML = saldoTot.toString();
+        limparCampoSoma();
+    }
+}
+function calcularImposto() {
+    if (saldoTot <= 1903.98) {
+        impostoTot = 0;
+    }
+    else if (saldoTot >= 1903.99 && saldoTot <= 2826.65) {
+        impostoTot = saldoTot * 7.5 / 100;
+    }
+    else if (saldoTot >= 2826.66 && saldoTot <= 3751.05) {
+        impostoTot = saldoTot * 15 / 100;
+    }
+    else if (saldoTot >= 3751.06 && saldoTot <= 4664.68) {
+        impostoTot = saldoTot * 22.5 / 100;
+    }
+    else {
+        impostoTot = saldoTot * 27.5 / 100;
+    }
+    return campoImposto.innerHTML = impostoTot.toString();
+}
+function limparCampoSoma() {
+    soma.value = " ";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTot = 0;
+        campoSaldo.innerHTML = saldoTot.toString();
+    }
+}
+function limparImposto() {
+    if (campoImposto) {
+        impostoTot = 0;
+        campoImposto.innerHTML = impostoTot.toString();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        somarAoSaldo(Number(soma.value));
+        calcularImposto();
+    });
+}
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+    limparImposto();
+});
